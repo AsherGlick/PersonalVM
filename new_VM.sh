14520d52ce8cec231e18699d556d6d1497b22dc2
Asher Glick <asher.glick@gmail.com>
2014-11-01 16:34:12 -0400

    updated the comments for the new VM script

--- new_VM.sh
+++ new_VM.sh
@@ -1,22 +1,57 @@
 #!/bin/bash
+#################################### LICENSE ###################################
+# Copyright (c) 2014, Asher Glick                                              #
+# All rights reserved.                                                         #
+#                                                                              #
+# Redistribution and use in source and binary forms, with or without           #
+# modification, are permitted provided that the following conditions are met:  #
+#                                                                              #
+# * Redistributions of source code must retain the above copyright notice,     #
+# this                                                                         #
+#   list of conditions and the following disclaimer.                           #
+# * Redistributions in binary form must reproduce the above copyright notice,  #
+#   this list of conditions and the following disclaimer in the documentation  #
+#   and/or other materials provided with the distribution.                     #
+#                                                                              #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    #
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   #
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE    #
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR          #
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF         #
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS     #
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN      #
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)      #
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   #
+# POSSIBILITY OF SUCH DAMAGE.                                                  #
+################################################################################
 
 
+
+################################ Welcome Message ###############################
+# Display a simple welcome message to the user telling them that they need to  #
+# run the command that this script prints in order to complete the             #
+# instalation of the new VM                                                    #
+################################################################################
 dialog --msgbox "\n     Building Install Commandfor a new VM\n      Command will be printed at the end" 8 50
 
-################################################################################
-# Name
-################################################################################
 
+
+########################## Get the name of the new VM ##########################
+# Prompt the user to enter the desired name of their new Virtual Machine       #
+################################################################################
 VM_name=`dialog --stdout --inputbox "New VM Name" 10 40`
 if [ -z "$VM_name" ]
 then 
 	exit
 fi
 
-################################################################################
-# Select instalation image
-################################################################################
 
+
+############################# Get the Install Image ############################
+# Prompt the user for which instal image they want to use from the             #
+# `/var/lib/libvirt/images/` folder                                            #
+################################################################################
 files=`ls /var/lib/libvirt/images/`
 
 dialog_options=()
@@ -25,18 +60,15 @@ dialog_options=()
 title="Pick an instalation image"
 window_height="30"
 menu_width="0"
-#window_width=$((`tput cols`-70))
-#window_width=$window_width - 2
 menu_height="30"
 
 for fn in $files; do
-    size=${#fn}
-    if [ $size -gt $menu_width ]; then
-        echo "larger"
-        menu_width=$size
-    fi
-    dialog_options+=($fn)
-    dialog_options+=("_")
+	size=${#fn}
+	if [ $size -gt $menu_width ]; then
+		menu_width=$size
+	fi
+	dialog_options+=($fn)
+	dialog_options+=("_")
 done
 
 # add the framing and padding to the width
@@ -50,9 +82,10 @@ fi
 
 
 
-###################
-# Get the OS Type #
-###################
+################################ Get the OS Type ###############################
+# Have the user select which general os type is being used, Linux, Unix, or    #
+# Windows. This is used for optimizations of the VM                            #
+################################################################################
 VM_OS_type=`dialog --stdout --menu "Pick the OS Type" 10 30 4 'linux' '_' 'unix' '_' 'windows' '_'`
 if [ -z "$VM_OS_type" ]
 then 
@@ -60,8 +93,11 @@ then
 fi
 
 
-################################################################################
-# Get the list of os variants #
+
+############################## Get the OS Varient ##############################
+# Get the variant of the OS that is being used. This provides further          #
+# optimizations for the VM. I am not sure what to do if the desired OS is not  #
+# on the list nor how this selection relates to the OS Type                    #
 ################################################################################
 IFS=$'\n'
 osvariants=(`virt-install --os-variant list`)
@@ -71,18 +107,18 @@ longest_name=0
 
 
 for variant in ${osvariants[@]};  do
-    tag=`echo "$variant" | sed -e "s/ .*//g"`
-    name="`echo \"$variant\" | sed -e \"s/.*: //g\"`"
-    tag_length=${#tag}
-    name_length=${#name}
-    if [ $tag_length -gt $longest_tag ]; then
-        longest_tag=$tag_length
-    fi
-    if [ $name_length -gt $longest_name ]; then
-        longest_name=$name_length
-    fi
-    variant_options+=($tag)
-    variant_options+=($name)
+	tag=`echo "$variant" | sed -e "s/ .*//g"`
+	name="`echo \"$variant\" | sed -e \"s/.*: //g\"`"
+	tag_length=${#tag}
+	name_length=${#name}
+	if [ $tag_length -gt $longest_tag ]; then
+		longest_tag=$tag_length
+	fi
+	if [ $name_length -gt $longest_name ]; then
+		longest_name=$name_length
+	fi
+	variant_options+=($tag)
+	variant_options+=($name)
 done
 
 
@@ -99,10 +135,9 @@ fi
 
 
 
-######################
-# Get ram allocation #
-######################
-
+########################## Get the RAM Allocation Size #########################
+# Prompt the user to enter how much RAM should be allocated to this VM (in MB) #
+################################################################################
 VM_RAM_allocation=`dialog --stdout --inputbox "Memory Allocated (MB)" 8 40 "512"`
 if [ -z "$VM_RAM_allocation" ]
 then 
@@ -110,10 +145,11 @@ then
 fi
 
 
-############################
-# Get HDD Space Allocation #
-############################
 
+###################### Get the Hard Drive Allocation Size ######################
+# Prompt the user to enter how much Hard Drive space should be allocated to    #
+# this VM (in GB)                                                              #
+################################################################################
 VM_HDD_allocation=`dialog --stdout --inputbox "Harddrive Allocation (GB)" 8 40 "20"`
 if [ -z "$VM_HDD_allocation" ]
 then 
@@ -122,25 +158,45 @@ fi
 
 
 ################################################################################
-# Display the command to the user so you can run it themselves
+################## Display the generated command to the user ###################
 ################################################################################
-
 echo "" # create some space
 echo "" # create some space
 echo virt-install \
-    --name=${VM_name} \
-    --arch=x86_64 \
-    --vcpus=1 \
-    --ram=${VM_RAM_allocation} \
-    --os-type=${VM_OS_type} \
-    --os-variant=${VM_variant} \
-    --hvm \
-    --connect=qemu:///system \
-    --network bridge:br0 \
-    --cdrom=/var/lib/libvirt/images/${VM_instalation_image} \
-    --disk path=/mnt/virtual_machines/${VM_name}.img,size=${VM_HDD_allocation} \
-    --graphics vnc,keymap=en-us \
-    --noautoconsole \
-    --accelerate
+	--name=${VM_name} \
+	--arch=x86_64 \
+	--vcpus=1 \
+	--ram=${VM_RAM_allocation} \
+	--os-type=${VM_OS_type} \
+	--os-variant=${VM_variant} \
+	--hvm \
+	--connect=qemu:///system \
+	--network bridge:br0 \
+	--cdrom=/var/lib/libvirt/images/${VM_instalation_image} \
+	--disk path=/mnt/virtual_machines/${VM_name}.img,size=${VM_HDD_allocation} \
+	--graphics vnc,keymap=en-us \
+	--noautoconsole \
+	--accelerate
 
 
+################################### SIGNATURE ##################################
+#                                      ,,                                      #
+#                     db             `7MM                                      #
+#                    ;MM:              MM                                      #
+#                   ,V^MM.    ,pP"Ybd  MMpMMMb.  .gP"Ya `7Mb,od8               #
+#                  ,M  `MM    8I   `"  MM    MM ,M'   Yb  MM' "'               #
+#                  AbmmmqMA   `YMMMa.  MM    MM 8M""""""  MM                   #
+#                 A'     VML  L.   I8  MM    MM YM.    ,  MM                   #
+#               .AMA.   .AMMA.M9mmmP'.JMML  JMML.`Mbmmd'.JMML.                 #
+#                                                                              #
+#                                                                              #
+#                                  ,,    ,,                                    #
+#                      .g8"""bgd `7MM    db        `7MM                        #
+#                    .dP'     `M   MM                MM                        #
+#                    dM'       `   MM  `7MM  ,p6"bo  MM  ,MP'                  #
+#                    MM            MM    MM 6M'  OO  MM ;Y                     #
+#                    MM.    `7MMF' MM    MM 8M       MM;Mm                     #
+#                    `Mb.     MM   MM    MM YM.    , MM `Mb.                   #
+#                      `"bmmmdPY .JMML..JMML.YMbmd'.JMML. YA.                  #
+#                                                                              #
+################################################################################
